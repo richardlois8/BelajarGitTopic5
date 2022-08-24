0471c738fa9d6db5c35be074e7a0faa87d083343
MSI\Irvan Wijaya <irfanwijayasardam@gmail.com>
2022-08-24 20:22:37 +0700

    Irvan COmmit  ,Creating ActivityIrvan

---
+++ app/src/main/java/com/example/belajargittopic5/ActivityIrvan.kt
@@ -0,0 +1,11 @@
+package com.example.belajargittopic5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityIrvan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_irvan)
+    }
+}
